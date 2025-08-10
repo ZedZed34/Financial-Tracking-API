51a75c3bd5f04aa45030c060de8fc0a01ed40ce6
ZedZed34 <haun070@aucklanduni.ac.nz>
2025-08-10 19:11:28 +0800

    I don't know

---
+++ src/main/java/com/zz/fintrack/HealthController.java
@@ -0,0 +1,12 @@
+package com.zz.fintrack;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String home() {
+        return "Financial-API is running ✅";
+    }
+}
