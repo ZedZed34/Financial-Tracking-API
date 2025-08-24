0c2f89adcc7d337bd931c4cc34403d3bf7b06d26
ZedZed34 <haun070@aucklanduni.ac.nz>
2025-08-25 05:12:24 +0800

    Workflow fixed

--- src/test/java/Finance/Tracking/Financial/API/FinancialApiApplicationTests.java
+++ src/test/java/Finance/Tracking/Financial/API/FinancialApiApplicationTests.java
@@ -1,9 +1,10 @@
 package Finance.Tracking.Financial.API;
 
+import com.zz.fintrack.FintrackApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FintrackApplication.class)
 class FinancialApiApplicationTests {
 
 	@Test
